89b12713a345ec8c897fc70d1b5cd86ea3248a18
akasulmin <kacper.kopacz.11@gmail.com>
2026-09-16 18:32:16 +0200

    Add files via upload

---
+++ kopacz_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 7".fontcolor("black").fontsize(3)+"<br>"); 
+document.write("Pierwszy wiersz to:10 Tak Nie 5".fontcolor("black").fontsize(3)+"<br>"); 
+document.write("Drugi wiersz to: 11 Tak Tak 5 ".fontcolor("black").fontsize(3)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("lime").bold().fontsize(5)+"<br>"); 
+document.write(document.lastModified.fontcolor("olive").strike().bold().fontsize(5)+"<br>"); 
